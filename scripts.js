e0e466f51a2720d73ccad4c4264d90aca31083ac
Christoph Simon - dotSource GmbH <csi@intranet.dotsource.de>
2023-01-22 03:10:32 +0100

    feat(homepage): initial commit - landing page

---
+++ scripts.js
@@ -0,0 +1,12 @@
+VANTA.CELLS({
+    el: "#vanta",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    color2: 0xfcfc97,
+    size: 1.30,
+    speed: 0.90
+});
